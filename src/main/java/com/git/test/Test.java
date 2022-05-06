08e5c9e62169166030844605f2dfaf16665fc64f
liyaqiu <361234567@qq.com>
2022-05-06 16:39:54 +0800

    修复bug2

--- src/main/java/com/git/test/Test.java
+++ src/main/java/com/git/test/Test.java
@@ -6,6 +6,6 @@ package com.git.test;
  **/
 public class Test {
     public static void main(String[] args) {
-        System.out.println("version2已经被我修复");
+        System.out.println("hello version2已经被我修复");
     }
 }
