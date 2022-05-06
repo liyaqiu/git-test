65803ddd119a7d11962ff9b978482d1ea3646a47
liyaqiu <361234567@qq.com>
2022-05-06 16:06:00 +0800

    version1

---
+++ src/main/java/com/git/test/Test.java
@@ -0,0 +1,11 @@
+package com.git.test;
+
+/**
+ * @author eric
+ * @date 2022/5/6 16:05
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("version1");
+    }
+}
