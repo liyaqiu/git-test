c7974ec6313d91ec497ff4bcd90bfede4a24b2c3
liyaqiu <361234567@qq.com>
2022-05-06 18:18:45 +0800

    master

---
+++ src/main/java/com/git/test/Test2.java
@@ -0,0 +1,20 @@
+package com.git.test;
+
+/**
+ * @author eric
+ * @date 2022/5/6 16:05
+ **/
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("a1");
+
+        System.out.println("master");
+
+        System.out.println("aaa");
+
+    }
+    public void test(){
+        
+    }
+}
+
