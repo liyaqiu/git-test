0e3329323f1d6288358d6360cd8dea7fca9ac858
liyaqiu <361234567@qq.com>
2022-05-06 16:06:39 +0800

    version2

--- src/main/java/com/git/test/Test.java
+++ src/main/java/com/git/test/Test.java
@@ -6,6 +6,6 @@ package com.git.test;
  **/
 public class Test {
     public static void main(String[] args) {
-        System.out.println("version1");
+        System.out.println("version2");
     }
 }
